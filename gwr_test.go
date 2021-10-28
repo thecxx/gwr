184d1837bd9d6209d80d41993b3437b150f46705
Kami <thecxx@126.com>
2021-10-28 21:24:16 +0800

    Update example

--- gwr_test.go
+++ gwr_test.go
@@ -10,8 +10,8 @@ import (
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 	// Params
 	var params struct {
-		Age     int    `wr-param:"{ url.age : $0<10||20<$0, # : \"参数校验失败\"}"`
-		FromUri string `wr-param:"{ header.from_uri : /^[0-9]+$/.match($0), # : \"参数校验失败\"}"`
+		Age     int    `wr-param:"{url.age: $0<10||20<$0, default: 20, #: \"参数校验失败\"}"`
+		FromUri string `wr-param:"{header.from_uri: /^[0-9]+$/.match($0), @default: \"https://www.baidu.com\", @error: \"参数校验失败\"}"`
 	}
 	wr := gwr.Attach(w, r)
 	// Auto flush
